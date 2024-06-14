1f6ce7aa33680ecedbe470d8bb5a171b097eac66
tigen <zusung47@gmail.com>
2024-06-14 17:51:32 +0900

    test commit

--- src/main/java/com/tigensoft/intern/dto/MessageDTO.java
+++ src/main/java/com/tigensoft/intern/dto/MessageDTO.java
@@ -1,6 +1,5 @@
 package com.tigensoft.intern.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
